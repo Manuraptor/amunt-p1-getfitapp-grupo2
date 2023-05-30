fde19da3f1bc91795cb311885f0d2f8853e7acf3
NellyKaykay <elelyonexclusive@gmail.com>
2023-05-30 12:38:26 +0200

    creating header and carousel components

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,9 @@
 </script>
 
 <main>
+<p>Somos GetFitEasy, una cadena de asesoramiento de salud y bienestar. 
+  En nuestro local ofrecemos un espacio de maquinas y hacemos clases
+  y seguimientos con los mejores especialistas. </p>
 
 </main>
 

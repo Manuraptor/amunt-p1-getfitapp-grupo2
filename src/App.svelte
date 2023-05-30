fd522d0c7a57780a86561079fa76a7ab55373f63
Manuraptor <majejuma.com>
2023-05-30 14:25:34 +0200

    nuestroprimercommit

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
-
+    import Servicios from './lib/servicios.svelte'
 </script>
 
 <main>
-
+<Servicios />
 </main>
 
 <style>
---
+++ src/lib/servicios.svelte
@@ -0,0 +1,10 @@
+<div class="tarjeta">
+<div class="fototarjeta">
+<img class="uno" src="src\assets\imagenes\fotoservicios\3.png">
+</div>
+
+<div class="textotarjeta">
+<h1> slhbsldbvdlsvbsvlsvlksvndslvlvsdlvslvl</h1>
+</div>
+</div>
+

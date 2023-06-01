dc6d06f05fadb0be34f272068510b4c68a6dccb6
NellyKaykay <elelyonexclusive@gmail.com>
2023-06-01 10:27:00 +0200

    header

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,39 @@
 <script>
- 
-
+  import logo from "./assets/imagenes/logo.png";
+  import MainText from "./components/MainText.svelte";
 </script>
 
 <main>
-<p>Somos GetFitEasy, una cadena de asesoramiento de salud y bienestar. 
-  En nuestro local ofrecemos un espacio de maquinas y hacemos clases
-  y seguimientos con los mejores especialistas. </p>
-
+  <header>
+    <img src={logo} alt="logo" />
+    <nav>
+      <span>Servicios</span>
+      <span>Tarifas</span>
+    </nav>
+  </header>
 </main>
 
-<style>
+<MainText />
 
+<style>
+  header {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: aqua;
+  }
+  nav {
+    padding: 20px;
+
+  }
+
+  span{
+    margin: 10px;
+    font-style: bold;
+  }
+  img {
+    width: 140px;
+    margin-left: 20px;
+  }
 </style>
---
+++ src/components/MainText.svelte
@@ -0,0 +1,21 @@
+
+<p>
+    Somos <span class="getfit">GetFitEasy</span>, una cadena de asesoramiento de
+    salud y bienestar. En nuestro local ofrecemos un espacio de maquinas y
+    hacemos clases y seguimientos con los mejores especialistas.
+  </p>
+
+  <style>
+    
+    p {
+    max-width: 650px;
+    margin: 120px auto;
+    text-align: center;
+  }
+
+  .getfit {
+    color: rgb(86, 195, 235);
+    font-weight: 600;
+  }
+
+  </style>

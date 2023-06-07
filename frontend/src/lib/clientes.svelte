0f9b144258e6a0832fbd63e0eb3556103b96593b
NellyKaykay <elelyonexclusive@gmail.com>
2023-06-07 12:13:04 +0200

    lista de clientes

---
+++ frontend/src/lib/clientes.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let name;
+    export let telNumber;
+    export let date;
+    export let email;
+    </script>
+    
+    
+    
+    <main>
+        <div class="client1">
+            <p>{name}</p>
+            <p>{telNumber}</p>
+            <p>{date}</p>
+            <p>{email}</p>
+        </div>
+    </main>
+    

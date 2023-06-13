e580b3f4f138ea75040e9a71930c391a47961043
NellyKaykay <elelyonexclusive@gmail.com>
2023-06-13 08:52:50 +0200

    style of clientes

--- frontend/src/lib/ClientesCard.svelte
+++ frontend/src/lib/ClientesCard.svelte
@@ -1,66 +1,106 @@
 <script>
-  export let title
-  export let text
-  export let imagen
-  export let name
-  export let birthDate
-  export let idCard
-  export let telNumber
-  export let email
-  export let address
-  export let startDate
-  export let renovationDate
-  export let billingDate
-  export let endDate
-  export let paymentType
-  export let subscriptionType
+  export let title;
+  export let text;
+  export let imagen;
+  export let name;
+  export let birthDate;
+  export let idCard;
+  export let telNumber;
+  export let email;
+  export let address;
+  export let startDate;
+  export let renovationDate;
+  export let billingDate;
+  export let endDate;
+  export let paymentType;
+  export let subscriptionType;
+</script>
 
+<main>
+  <div class="card">
+    <div class="image">
+      <img src={imagen} />
+      <p>{name}</p>
+    </div>
+   
+    <div class="personal">
+      <div>
+        <h2>{title}</h2>
+       </div>
+      <div class="texto">
+      <p>{birthDate}</p>
+      <p>{idCard}</p>
+      <p>{telNumber}</p>
+      <p>{email}</p>
+      <p>{address}</p>
+    </div>
+    </div>
 
-    </script>
+    <div class="billing">
+      <div><h2>{text}</h2>
+      </div>
+      <div class="texto">
+      <p>{startDate}</p>
+      <p>{renovationDate}</p>
+      <p>{billingDate}</p>
+      <p>{endDate}</p>
+      <p>{paymentType}</p>
+      <p>{subscriptionType}</p>
+    </div>
     
-       
-    <main>
+    </div>
+  </div>
+</main>
 
-      <div class="card">
-               
-        <div class="image">
-          <img src={imagen}>
-          <p>{name}</p>
-                   
-          </div>
+<style>
 
-          <div class="personal">
-            <h2>{title}</h2>
-            <p>{birthDate}</p>
-            <p>{idCard}</p>
-            <p>{telNumber}</p>
-            <p>{email}</p>
-            <p>{address}</p>
-          </div>
+ 
+  .card {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 50px;
+    font-size: small;
+    background-color: rgb(245, 246, 248);
+    margin-right: 20px;
+    border: none;
+    gap: 10px;
+  }
 
-          <div class="billing">
-            <h2>{text}</h2>
-            <p>{startDate}</p>
-            <p>{renovationDate}</p>
-            <p>{billingDate}</p>
-            <p>{endDate}</p>
-            <p>{paymentType}</p>
-            <p>{subscriptionType}</p>
+  img {
+    margin-top: 50px;
+    height: 160px;
+    width: 160px;
 
-          </div>
+  }
 
-          </div>
-
-      
-    </main>
-
-    <style>
-
-      
+  .texto{
+    background-color: white;
+   
+    border-radius: 8%;
     
+  }
+.personal{
+  width: 35%;
+ 
+}
+ h2{
+  margin-top: 20px;
+  width: 55%;
+  font-size: medium;
+  
+  color: aliceblue;
+  padding: 0.8em;
+  background-color:  #0bcbf6;
+ 
+ }
 
-     img{
-      height: 80px;
-      width: 80px;
-     }
-    </style>
+ .image{
+  width: 20%;
+ }
+
+ .billing{
+  width: 40%;
+ 
+ }
+</style>

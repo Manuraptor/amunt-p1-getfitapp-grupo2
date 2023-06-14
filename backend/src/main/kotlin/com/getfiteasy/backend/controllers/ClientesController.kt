2c3421a68bd584144681ef6f6d771bc5fed4ab7e
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-06-14 12:44:20 +0200

    crear data sql

--- backend/src/main/kotlin/com/getfiteasy/backend/controllers/ClientesController.kt
+++ backend/src/main/kotlin/com/getfiteasy/backend/controllers/ClientesController.kt
@@ -1,4 +1,20 @@
 package com.getfiteasy.backend.controllers
 
-class ClientesController {
+import com.getfiteasy.backend.repositories.ClientesRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class ClienteController (private val repository: ClientesRepository){
+
+    @GetMapping("/clientes")
+    fun allClientes() = repository.findAll()
+
+
+
 }
+
+
+
+
--- backend/src/main/kotlin/com/getfiteasy/backend/repositories/ClientesRepository.kt
+++ backend/src/main/kotlin/com/getfiteasy/backend/repositories/ClientesRepository.kt
@@ -1,4 +1,8 @@
 package com.getfiteasy.backend.repositories
 
-interface ClientesRepository {
-}
+import com.getfiteasy.backend.domain.Client
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ClientesRepository : JpaRepository<Client, Long>

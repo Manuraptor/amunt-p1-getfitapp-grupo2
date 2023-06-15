d1ed9886f53b7a0d639710f99e11437a0b7fe780
jutelma <jutelma07051980@gmail.com>
2023-06-15 12:25:18 +0200

    coneccion front back

--- backend/src/main/kotlin/com/getfiteasy/backend/controllers/ClientesController.kt
+++ backend/src/main/kotlin/com/getfiteasy/backend/controllers/ClientesController.kt
@@ -1,15 +1,24 @@
 package com.getfiteasy.backend.controllers
 
+import com.getfiteasy.backend.domain.Client
 import com.getfiteasy.backend.repositories.ClientesRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
+@CrossOrigin
 class ClienteController (private val repository: ClientesRepository){
 
     @GetMapping("/clientes")
-    fun allClientes() = repository.findAll()
+    fun allSubscribers() : List<Client>? {
+        return repository.findAll()
+
+    }
+    @GetMapping("/clientes/location/{location}")
+    fun getSubscribersByLocation(@PathVariable location: String): List<Client>? {
+        return repository.findAllByAddressContains(location)
+    }
+
 
 
 
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into clientes(id, name, birth_date, id_card, tel_number, email, address, lopd_accepted, start_date,
                      renovation_date, billing_date, end_date, payment_type, subscription_type)
 values (1, 'Nelly Dominguez', '18/09/1983', '34567854G', '698675345', 'nelly.dom@gmail.com',
-        'Av/Diagonal 124, Barcelona', true, '12/05/2021', '23/12/2023', '05/07/2023', '23/12/2023', 'efectivo',
-        'Annual'),
+        'Av/Diagonal 124, Barcelona', true, '12/05/2021', '23/12/2023', '05/07/2023', '23/12/2023', 'Efectivo',
+        'Anual'),
        (2, 'Manuel Rodrígues', '01/12/1995', '323443256h', '6781254365', 'manu.rod@gmail.com',
-        'C/Valencia 45, Barcelona', false, '16/05/2023', '16/08/2023', '05/07/2023', null, 'tarjeta', 'trimestral'),
+        'C/Valencia 45, Barcelona', false, '16/05/2023', '16/08/2023', '05/07/2023', null, 'tarjeta', 'Trimestral'),
        (3, 'Federico Martínez', '24/11/1993', '45665478j', '695756098', 'fed.martm@gmail.com',
         'C/Mallorca 226, Barcelona', true, '08/10/2022', '08/10/2023', '05/07/2023', null, 'efectivo', 'Mensual');
